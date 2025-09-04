3b9127685e6943942b230f7f246d49705803de5b
AkashHosur <akashch1704@gmail.com>
2025-09-04 18:48:42 +0530

    Add files via upload

---
+++ DAY_ONE/VaribleDeclarationandInitioalization.java
@@ -0,0 +1,16 @@
+class VaribleDeclarationandInitioalization
+{
+	public static void main(String [] args)
+	{
+
+	//variable declaration and initializtion
+		int id=101;
+		String name="Akash";
+		double salary=2.55;
+	//print ID,NAME,SALARY
+		System.out.println("INFORMATION EMPLOYEE  :");
+		System.out.println("ID OF EMPLOYEE IS : "+id);
+		System.out.println("NAME OF EMPLOYEE IS : "+name);
+		System.out.println("SALARY OF EMPLOYEE  IS : "+salary);
+	}
+}
---
+++ DAY_ONE/student.java
@@ -0,0 +1,18 @@
+class student
+{
+	public static void main(String [] args)
+	{
+          //variable declaration
+	int age;
+	//variable initialization
+	age=23;
+	String name="Akash";
+
+	System.out.println(age);
+	System.out.println(name);
+
+	System.out.println("________");
+	System.out.println("AGE IS :"+age);
+	System.out.println("NAME IS :"+name);
+	}
+}
