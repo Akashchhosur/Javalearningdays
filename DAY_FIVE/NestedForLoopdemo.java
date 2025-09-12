5a1b53555afb545005211e10d2f45c76718e7c98
AkashHosur <akashch1704@gmail.com>
2025-09-12 21:08:47 +0530

    Add files via upload

---
+++ DAY_FIVE/NestedForLoopdemo.java
@@ -0,0 +1,13 @@
+class NestedForLoopdemo
+{
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=6;j<=7;j++)
+			{	
+				System.out.println(i+"  "+j);
+			}	
+		}
+	}
+}
---
+++ DAY_FIVE/ReverseNumber.java
@@ -0,0 +1,11 @@
+class ReverseNumber
+{
+ 	public static void main(String[] agrs)
+	{
+		for(int i=5;i>=1;i--)
+		{
+			System.out.println(i);
+		}
+
+	}
+}
