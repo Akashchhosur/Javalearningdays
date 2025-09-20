7ffb1b72c500a4a6a479ee64be82b837efa6c208
AkashHosur <akashch1704@gmail.com>
2025-09-20 15:46:26 +0530

    Add files via upload

---
+++ StaticPrograms/com/Employee.java
@@ -0,0 +1,14 @@
+package com;
+
+class Employee {
+	int id=214;
+	String name="Akash";
+	double sal=99999.999;
+	public static void main(String[] args) {
+		Employee em =new Employee();
+		System.out.println(em.id);
+		System.out.println(em.name);
+		System.out.println(em.sal);
+	}
+	
+}
---
+++ StaticPrograms/com/laptop.java
@@ -0,0 +1,8 @@
+package com;
+
+class laptop {
+	int cost=500000000;
+	String brand="hp";
+	
+
+}
